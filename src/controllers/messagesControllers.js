cea128f4ae604dbaf89ef874d5211dfb6409656b
khadar m dagal <khmdagal@gmail.com>
2025-04-23 12:00:26 +0100

    Uncomment-ed out

--- src/controllers/messagesControllers.js
+++ src/controllers/messagesControllers.js
@@ -1,13 +1,13 @@
 const pool = require('../utilis/db');
 
 exports.getMessages = async (req, res, next) => {
-    // try {
-    //     const messages = (await pool.query('SELECT * FROM messages')).rows
-    //     res.status(200).json({
-    //         status: 'success',
-    //         result: messages
-    //     })
-    // } catch (error) {
-    //     next(error)
-    // }
+    try {
+        const messages = (await pool.query('SELECT * FROM messages')).rows
+        res.status(200).json({
+            status: 'success',
+            result: messages
+        })
+    } catch (error) {
+        next(error)
+    }
 }
