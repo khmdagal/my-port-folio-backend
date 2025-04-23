aad028f5a8770175b2015e8628ce4b8d8fc9ffbc
khadar m dagal <khmdagal@gmail.com>
2025-04-23 11:54:17 +0100

    Write business for the projects just enought to make the test to pass

--- src/controllers/projectsControllers.js
+++ src/controllers/projectsControllers.js
@@ -1,5 +1,13 @@
 const pool = require('../utilis/db');
 
 exports.getProjects = async (req, res, next) => {
-
+    try {
+        const projects = (await pool.query('SELECT * FROM projects')).rows
+        res.status(200).json({
+            status: 'success',
+            result: projects
+        })
+    } catch (error) {
+        next(error)
+    }
 }
