ab242a1954fd5e426ab7f805207a6191af7464b9
khadar m dagal <khmdagal@gmail.com>
2025-04-24 11:51:22 +0100

    Remove unnecessaary empty line

--- src/controllers/collaborationToolsControllers.js
+++ src/controllers/collaborationToolsControllers.js
@@ -3,8 +3,6 @@ const pool = require('../utilis/db');
 exports.getCollaborationTools = async (req, res, next) => {
     try {
         const collaborationTools = (await pool.query('SELECT * FROM collaborations')).rows
-
-       
         res.status(200).json({
             status: 'success',
             result: collaborationTools
