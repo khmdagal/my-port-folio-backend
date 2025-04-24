88c0ed7abbaf0ab4fb91181878eb7d5b464eaef5
khadar m dagal <khmdagal@gmail.com>
2025-04-24 11:38:01 +0100

    Implement skills logic just to make the test pass

--- src/controllers/skillsControllers.js
+++ src/controllers/skillsControllers.js
@@ -1,5 +1,13 @@
 const pool = require('../utilis/db')
 
 exports.getSkills = async (req, res, next) => {
-
-}
+    try {
+        const skills = (await pool.query('SELECT * FROM skills')).rows
+        res.status(200).json({
+            status: 'success',
+            result: skills
+        })
+    } catch (error) {
+        next(error)
+    }
+} 
