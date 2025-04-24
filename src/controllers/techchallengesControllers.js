2f065a16cec1cd799801caead2a967d37450b13f
khadar m dagal <khmdagal@gmail.com>
2025-04-24 12:02:20 +0100

    Add tech challenges implementation

--- src/controllers/techchallengesControllers.js
+++ src/controllers/techchallengesControllers.js
@@ -1,5 +1,13 @@
 const pool = require('../utilis/db')
 
 exports.getTechChallenges = async (req, res, next) => {
-
+    try {
+        const skills = (await pool.query('SELECT * FROM techchallenges')).rows
+        res.status(200).json({
+            status: 'success',
+            result: skills
+        })
+    } catch (error) {
+        next(error)
+    }
 }
