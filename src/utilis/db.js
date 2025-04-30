844e50de51ce26b199d780160bbdbbbc520052c6
khadar m dagal <khmdagal@gmail.com>
2025-04-30 14:39:40 +0100

    Comment out SSL requirement

--- src/utilis/db.js
+++ src/utilis/db.js
@@ -9,10 +9,10 @@ const pool = new Pool({
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
-    // ssl: {
-    //     require: true,
-    //     rejectUnauthorized: true,
-    // },
+    ssl: {
+        require: true,
+        rejectUnauthorized: true,
+    },
 });
 
 module.exports = pool;
