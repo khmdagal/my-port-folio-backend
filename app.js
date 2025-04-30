c31567f60a53a7fc27bb62c9e37411f664abc1f9
khadar m dagal <khmdagal@gmail.com>
2025-04-30 12:06:47 +0100

    Change app file directory

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const collaborationsRoutes = require('./routes/collaborationRoutes');
-const messagesRoutes = require('./routes/messagesRoutes');
-const projectsRoutes = require('./routes/projectsRoutes');
-const skillsRoutes = require('./routes/skillsRoutes');
-const techchallengesRoutes = require('./routes/techchallengesRoutes');
-const feedbackRoutes = require('./routes/feedbackRoutes');
-const aboutMeRoutes = require('./routes/aboutMeRoutes')
+const collaborationsRoutes = require('./src/routes/collaborationRoutes');
+const messagesRoutes = require('./src/routes/messagesRoutes');
+const projectsRoutes = require('./src/routes/projectsRoutes');
+const skillsRoutes = require('./src/routes/skillsRoutes');
+const techchallengesRoutes = require('./src/routes/techchallengesRoutes');
+const feedbackRoutes = require('./src/routes/feedbackRoutes');
+const aboutMeRoutes = require('./src/routes/aboutMeRoutes')
 
 
 app.use(express.json());
@@ -16,7 +16,7 @@ app.use(cors());
 
 app.use((req, res, next) => {
     next()
-    console.log({ res })
+    console.log(req.baseUrl, req.headers['user-agent'])
 })
 //APIs
 
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
-const app = require('./src/app');
+const app = require('./app');
 const port = 3500;
 
 app.listen(port, () => console.log(`server is up and running by prot ${port}`))
